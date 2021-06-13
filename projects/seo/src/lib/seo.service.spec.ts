19abe87bd01f7a91f8edb4cdf20a20964dc07250
Rabyâ Raghib <rabraghib@gmail.com>
2021-06-13 10:37:12 +0100

    Use ESLint instead of tslint

--- projects/seo/src/lib/seo.service.spec.ts
+++ projects/seo/src/lib/seo.service.spec.ts
@@ -3,15 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { SeoService } from './seo.service';
 
 describe('SeoService', () => {
-    let service: SeoService;
+  let service: SeoService;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(SeoService);
-    });
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SeoService);
+  });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
-    
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
