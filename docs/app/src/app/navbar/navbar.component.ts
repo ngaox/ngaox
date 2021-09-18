1fbf349b3dfbf49806a3f2efd5b212b806df8cc9
Rabyâ Raghib <rabraghib@gmail.com>
2021-09-18 21:26:53 +0100

    fix(docs): navbar responsiveness

--- docs/app/src/app/navbar/navbar.component.ts
+++ docs/app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'docs-navbar',
@@ -6,11 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  // isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  //   .pipe(
-  //     map(result => result.matches),
-  //     shareReplay()
-  //   );
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  constructor() {}
+  readonly SECTIONS = [
+    {
+      path: '/docs',
+      name: 'Docs'
+    },
+    {
+      path: '/docs/schematics',
+      name: 'Schematics'
+    },
+    {
+      path: '/guide',
+      name: 'Guides'
+    }
+  ];
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
