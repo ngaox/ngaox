b25bc5fb86282403bc456fcbb021aa95757c3ef5
Rabyâ Raghib <rabraghib@gmail.com>
2021-09-20 17:36:43 +0100

    feat(docs): add default markdown styling

--- docs/app/src/app/pages/docs/viewer/viewer.component.ts
+++ docs/app/src/app/pages/docs/viewer/viewer.component.ts
@@ -1,13 +1,19 @@
-import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
 import { HighlightService } from '../../../core/highlight.service';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { map, mergeMap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'docs-viewer',
   templateUrl: './viewer.component.html',
-  styleUrls: ['./viewer.component.scss']
+  styleUrls: ['./viewer.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ViewerComponent implements AfterViewChecked, OnInit {
   content$?: Observable<string>;
