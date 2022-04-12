8adcd88891bb5e2d16e5d6a41907ea05b29a5d18
Kaname <56084970+kaname-png@users.noreply.github.com>
2022-04-11 18:14:50 -0600

    chore: prettier format

--- packages/seo/src/lib/seo.service.ts
+++ packages/seo/src/lib/seo.service.ts
@@ -1,15 +1,15 @@
-import { DOCUMENT } from "@angular/common";
-import { Inject, Injectable, Optional } from "@angular/core";
-import { Meta, MetaDefinition, Title } from "@angular/platform-browser";
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable, Optional } from '@angular/core';
+import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
 import {
   IPageSeoData,
   ISeoImage,
   ISeoTwitter,
   SeoDefaultsToken,
-} from "./shared/modals";
+} from './shared/modals';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class SeoService {
   constructor(
@@ -39,37 +39,37 @@ export class SeoService {
     if (seoData.twitter) this.setTwitter(seoData.twitter);
     if (seoData.fbAppId) this.setFbAppId(seoData.fbAppId);
     if (seoData.siteName) this.setSiteName(seoData.siteName);
-    if (seoData.extra?.["theme-color"])
-      this.setThemeColor(seoData.extra["theme-color"]);
+    if (seoData.extra?.['theme-color'])
+      this.setThemeColor(seoData.extra['theme-color']);
   }
 
   public setTitle(title: string): void {
     this.title.setTitle(title);
     this.generateTags([
-      { property: "og:title", content: title },
-      { name: "twitter:title", content: title },
-      { name: "title", content: title },
+      { property: 'og:title', content: title },
+      { name: 'twitter:title', content: title },
+      { name: 'title', content: title },
     ]);
   }
 
   public setThemeColor(color: string): void {
-    this.generateTags([{ name: "theme-color", content: color }]);
+    this.generateTags([{ name: 'theme-color', content: color }]);
   }
 
   public setKeywords(keywords: string): void {
-    this.generateTags([{ name: "keywords", content: keywords }]);
+    this.generateTags([{ name: 'keywords', content: keywords }]);
   }
 
   public setDescription(description: string): void {
     this.generateTags([
-      { name: "description", content: description },
-      { property: "og:description", content: description },
-      { name: "twitter:description", content: description },
+      { name: 'description', content: description },
+      { property: 'og:description', content: description },
+      { name: 'twitter:description', content: description },
     ]);
   }
 
   public setUrl(url: string): void {
-    this.generateTags([{ property: "og:url", content: url }]);
+    this.generateTags([{ property: 'og:url', content: url }]);
     // set canonical link
     const oldElement = this.document.head.querySelector(
       `link[rel='canonical']`
@@ -77,53 +77,53 @@ export class SeoService {
     if (oldElement) {
       this.document.head.removeChild(oldElement);
     }
-    const link: HTMLLinkElement = this.document.createElement("link");
-    link.setAttribute("rel", "canonical");
-    link.setAttribute("href", url);
+    const link: HTMLLinkElement = this.document.createElement('link');
+    link.setAttribute('rel', 'canonical');
+    link.setAttribute('href', url);
     this.document.head.appendChild(link);
   }
 
   public setType(type: string): void {
-    this.generateTags([{ property: "og:type", content: type }]);
+    this.generateTags([{ property: 'og:type', content: type }]);
   }
 
   public setImage(image: string | ISeoImage): void {
-    if (typeof image === "string") {
+    if (typeof image === 'string') {
       this.generateTags([
-        { property: "og:image", content: image },
-        { name: "twitter:image", content: image },
-        { property: "image", content: image },
+        { property: 'og:image', content: image },
+        { name: 'twitter:image', content: image },
+        { property: 'image', content: image },
       ]);
     } else {
       this.generateTags([
-        { property: "og:image", content: image.url },
-        { name: "twitter:image", content: image.url },
-        { property: "image", content: image.url },
+        { property: 'og:image', content: image.url },
+        { name: 'twitter:image', content: image.url },
+        { property: 'image', content: image.url },
       ]);
-      if (image.url.startsWith("https")) {
+      if (image.url.startsWith('https')) {
         this.generateTags([
-          { property: "og:image:secure_url", content: image.url },
+          { property: 'og:image:secure_url', content: image.url },
         ]);
       }
       if (image.alt) {
         this.generateTags([
-          { property: "twitter:image:alt", content: image.alt },
-          { property: "og:image:alt", content: image.alt },
+          { property: 'twitter:image:alt', content: image.alt },
+          { property: 'og:image:alt', content: image.alt },
         ]);
       }
       if (image.height) {
         this.generateTags([
-          { property: "og:image:height", content: image.height.toString() },
+          { property: 'og:image:height', content: image.height.toString() },
         ]);
       }
       if (image.width) {
         this.generateTags([
-          { property: "og:image:width", content: image.width.toString() },
+          { property: 'og:image:width', content: image.width.toString() },
         ]);
       }
       if (image.width) {
         this.generateTags([
-          { property: "og:image:type", content: `${image.mimeType}` },
+          { property: 'og:image:type', content: `${image.mimeType}` },
         ]);
       }
     }
@@ -131,22 +131,22 @@ export class SeoService {
 
   public setTwitter(twitterData: ISeoTwitter): void {
     if (twitterData.site) {
-      this.generateTags([{ name: "twitter:site", content: twitterData.site }]);
+      this.generateTags([{ name: 'twitter:site', content: twitterData.site }]);
     }
     if (twitterData.creator) {
       this.generateTags([
-        { name: "twitter:creator", content: twitterData.creator },
+        { name: 'twitter:creator', content: twitterData.creator },
       ]);
     }
     if (twitterData.card) {
-      this.generateTags([{ name: "twitter:card", content: twitterData.card }]);
+      this.generateTags([{ name: 'twitter:card', content: twitterData.card }]);
     }
   }
   public setFbAppId(Id: string): void {
-    this.generateTags([{ property: "fb:app_id", content: Id }]);
+    this.generateTags([{ property: 'fb:app_id', content: Id }]);
   }
 
   public setSiteName(siteName: string): void {
-    this.generateTags([{ property: "og:site_name", content: siteName }]);
+    this.generateTags([{ property: 'og:site_name', content: siteName }]);
   }
 }
